85866959dfbf6740a7363e5e525ebd59c3046d0f
median-man <johndrunner@yahoo.com>
2017-08-14 21:44:00 -0700

    added function to log game output

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,20 +7,8 @@
 function renderGame(arrWord, remGuesses, wins, usedLetters) {
 
 
-	console.log(arrWord);
-	console.log(typeof arrWord.join);
-	var s = arrWord.join(" ");
-	// display current solution ( "a _ _" )
-	if ( arrWord.length > 0 ) {
-		document.getElementById("word").textContent = "hello";
-		
-	}
-	// display the number of guesses remaining
-	// display number of wins
-	// display the letters already guessed	
-
-
+	console.log("word: " + arrWord.join(" "));
+	console.log("guesses rem: " + remGuesses);
+	console.log("wins: " + wins);
+	console.log("letters: " + usedLetters);
 }
-
-var testWord = "hello";
-renderGame( testWord.split(""), " ", " ", " " );
