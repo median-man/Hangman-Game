1ed0fb14ded255a56bd62ab381f0daeddccce0df
median-man <johndrunner@yahoo.com>
2017-08-17 07:46:26 -0700

    added alt text for images in data

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -2,47 +2,59 @@
 var data = [
 	{
 		word: "Yosemite",
-		image: "yosemite-1.jpg"
+		image: "yosemite-1.jpg",
+		alt: "View of Yosemite valley. Towering granite cliffs on left and right. Evergreen trees along base of cliffs. Pond reflecting blue sky in foreground."
 	},
 	{
 		word: "Acadia",
-		image: "acadia-1.jpg"
+		image: "acadia-1.jpg",
+		alt: "Rocky Maine coast covered in trees. Partly cloudy skies."
 	},
 	{
 		word: "bison",
-		image: "bison.jpg"
+		image: "bison.jpg",
+		alt: "Profile of front the front half of a bison to right. Grass in background."
 	},
 	{
 		word: "moose",
-		image: ""
+		image: "",
+		alt: ""
 	},
 	{
 		word: "mountain",
-		image: ""
+		image: "",
+		alt: ""
 	},
 	{
 		word: "Olympic",
-		image: "olympic-01.jpg"
+		image: "olympic-01.jpg",
+		alt: "Calm body of water reflecting a mostly cloudy sky. Forested mountain to the right."
 	},
 	{
 		word: "Pinnacles",
-		image: ""
+		image: "",
+		alt: ""
 	},
 	{
 		word: "Arches",
-		image: "arches-1.jpg"
+		image: "arches-1.jpg",
+		alt: "Rocky arch with an orange-brown hue frames another rocky structure in the background. Moderate amount of snow blankets gently sloping surfaces."
 	},
 	{
 		word: "Glacier",
-		image: "glacier-1.jpg"
+		image: "glacier-1.jpg",
+		alt: "Snowy gray and brown cliffs. Turquoise pool with large ice bodies suspended in it in the foreground. Broken clouds in the sky."
+
 	},
 	{
 		word: "Kitmai",
-		image: "kitmai-1.jpg"
+		image: "kitmai-1.jpg",
+		alt: "Pastoral mountain panorama."
 	},
 	{
 		word: "Rockies",
-		image: "rocky-mountain-1.jpg"
+		image: "rocky-mountain-1.jpg",
+		alt: "Lake with reflections of sky and rocky mountains. Evergreen trees along lake shore. Tall rocky mountains on horizon with 30% white snow coverage."
 	}
 ];
 
